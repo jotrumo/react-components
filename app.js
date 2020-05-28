ec63fd8b2e5f5370cb36a05442dfdd0557f2b100
Joseph Montoya <jtmontoya26@gmail.com>
2020-05-28 11:25:03 -0700

    Quick Save - redone due to file error

--- app.js
+++ app.js
@@ -2,15 +2,42 @@
 var App = () => (
   <div>
     <h2>My Grocery List</h2>
-    <GroceryList groceryListItem={['Apples', "Cucumber", "Coffee"]}/>
+    <GroceryList groceryListItems={['Apples', "Cucumber", "Coffee"]}/>
   </div>
 );
 
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.groceryListItem}</li>
+    );
+  }
+}
+
 var GroceryList = (props) => (
   <ul>
-    <li>{props.groceryListItem[0]}</li>
-    <li>{props.groceryListItem[1]}</li>
-    <li>{props.groceryListItem[2]}</li>
+    {props.groceryListItems.map(groceryListItem =>
+      <GroceryListItem groceryListItem={groceryListItem} />
+      )}
   </ul>
 );
 
