65a8e92619f2ebdd9cce0c4637384318d6fe0908
Joseph Montoya <jtmontoya26@gmail.com>
2020-05-28 11:06:10 -0700

    Quick Save

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryListItem={['Apples', "Cucumber", "Coffee"]}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    <li>{props.groceryListItem[0]}</li>
+    <li>{props.groceryListItem[1]}</li>
+    <li>{props.groceryListItem[2]}</li>
+  </ul>
+);
+
+// var Cucumber = () => (
+//   <div>Cucumber!</div>
+// );
+
+// var Apples = () => (
+//   <div>Apples!</div>
+// );
+
+ReactDOM.render(<App />, document.getElementById("app"));
